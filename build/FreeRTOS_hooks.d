b044c6a44aeae7641f6556777aa69a6b12b51ca5
kissellco <kissellco@users.noreply.github.com>
2025-02-26 22:14:10 +1100

    Amended build process

---
+++ build/FreeRTOS_hooks.d
@@ -0,0 +1,20 @@
+/home/rhys/freeRTOS78000/build/FreeRTOS_hooks.o: src/FreeRTOS_hooks.c \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ include/FreeRTOSConfig.h \
+ /home/rhys/msdk/Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ /home/rhys/msdk/Libraries/CMSIS/5.9.0/Core/Include/core_cm4.h \
+ /home/rhys/msdk/Libraries/CMSIS/5.9.0/Core/Include/cmsis_version.h \
+ /home/rhys/msdk/Libraries/CMSIS/5.9.0/Core/Include/cmsis_compiler.h \
+ /home/rhys/msdk/Libraries/CMSIS/5.9.0/Core/Include/cmsis_gcc.h \
+ /home/rhys/msdk/Libraries/CMSIS/5.9.0/Core/Include/mpu_armv7.h \
+ /home/rhys/msdk/Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ /home/rhys/msdk/Libraries/CMSIS/Device/Maxim/MAX78000/Include/gcr_regs.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/projdefs.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/portable.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/deprecated_definitions.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/mpu_wrappers.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/task.h \
+ /home/rhys/msdk/Libraries/FreeRTOS//Source/include/list.h
